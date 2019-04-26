0779c0e78558fbeb8a70deecb0bed5a65ccd16cd
ann grynchuk <ann.grynchuk7@gmail.com>
2019-04-26 21:16:31 +0300

    Updates

---
+++ precache-manifest.c84c792e52fcfd4dda9dd9e6f727e774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66a226c2732e1fba8026",
+    "url": "/React-Friends-App/static/js/runtime~main.5d78d5ac.js"
+  },
+  {
+    "revision": "c34beb5b37d5c1de753b",
+    "url": "/React-Friends-App/static/js/main.22636752.chunk.js"
+  },
+  {
+    "revision": "274582576ae41c0a1950",
+    "url": "/React-Friends-App/static/js/2.c2e3dd46.chunk.js"
+  },
+  {
+    "revision": "c34beb5b37d5c1de753b",
+    "url": "/React-Friends-App/static/css/main.d31f6079.chunk.css"
+  },
+  {
+    "revision": "f05696c0d7cbf55d09d90eaf71444123",
+    "url": "/React-Friends-App/index.html"
+  }
+];
